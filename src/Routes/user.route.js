e2ab8e2fa787ee60318dba86d2d37cfc5452efa7
dinesh0085 <105920688+dinesh0085@users.noreply.github.com>
2023-01-24 14:16:11 +0530

    added job backend

--- src/Routes/user.route.js
+++ src/Routes/user.route.js
@@ -33,7 +33,7 @@ app.post("/signin",async(req,res)=>{
         const token=jwt.sign({id:user._id,name:user.name,email:user.email,type:user.type},secretKey,{
             expiresIn:"1 day"
         })
-      res.send({msg:"User login successfull",token})
+      res.send({msg:"User login successfull",user,token})
      }else{
         res.send({msg:"User login failed",error:"User not found"})
      }
